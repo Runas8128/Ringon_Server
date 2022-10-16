558ef7061103fc9b8d6cfd806083b461eb6e2739
Runas8128 <runas8128@naver.com>
2022-10-16 18:28:23 +0900

    use axios instead of native http module

--- commands/deck/portallink.js
+++ commands/deck/portallink.js
@@ -1,4 +1,4 @@
-const https = require('https');
+const axios = require('axios');
 const { SlashCommandBuilder, ChatInputCommandInteraction } = require('discord.js');
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { reply } = require('../../util');
@@ -19,41 +19,30 @@ module.exports = {
     await interaction.deferReply();
     const deck_code = interaction.options.getString('덱코드');
 
-    // GET 'https://shadowverse-portal.com/api/v1/deck/import'
-    // params={'format': 'json', 'deck_code': deck_code}
-    // data = resp.json().data
+    /**
+     *  @typedef Portal
+     *    @property {object} data
+     *      @property {string} data.hash
+     *      @property {any[]} data.errors
+     */
+    /** @type {Portal} */
+    const result = (await axios.get(
+      `https://shadowverse-portal.com/api/v1/deck/import?format=json&deck_code=${deck_code}`)).data;
 
-    https.get(
-      `https://shadowverse-portal.com/api/v1/deck/import?format=json&deck_code=${deck_code}`,
-      (resp) => {
-        let rst = '';
-
-        resp.on('data', (chunk) => {
-          rst += chunk;
-        });
-        resp.on('error', (err) => {
-          logger.error(err);
-        });
-        resp.on('end', async () => {
-          const deck_json = JSON.parse(rst).data;
-          if (Object.keys(deck_json.errors).length > 0) {
-            await reply(
-              interaction,
-              '덱 코드가 무효하거나, 잘못 입력되었습니다. 다시 입력해 주시기 바랍니다.',
-            );
-          }
-          else {
-            const row = new ActionRowBuilder()
-              .addComponents(
-                new ButtonBuilder()
-                  .setLabel('포탈 링크')
-                  .setStyle(ButtonStyle.Link)
-                  .setURL(`https://shadowverse-portal.com/deck/${deck_json.hash}?lang=ko`),
-              );
-            await reply(interaction, { components: [row] });
-          }
-        });
-      },
-    ).end();
+    if (result.data.errors.length > 0) {
+      await reply(
+        interaction,
+        '덱 코드가 무효하거나, 잘못 입력되었습니다. 다시 입력해 주시기 바랍니다.',
+      );
+      return;
+    }
+    const row = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setLabel('포탈 링크')
+          .setStyle(ButtonStyle.Link)
+          .setURL(`https://shadowverse-portal.com/deck/${result.data.hash}?lang=ko`),
+      );
+    await reply(interaction, { components: [row] });
   },
 };
