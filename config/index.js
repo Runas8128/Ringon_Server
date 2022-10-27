80e6d08de1e8ce45efccb32beba1568ccebd626b
Runas8128 <runas8128@naver.com>
2022-10-27 19:24:15 +0900

    add more info msg (to check testing flag)

--- config/index.js
+++ config/index.js
@@ -1,9 +1,16 @@
 const config_common = require('./config_common.json');
+const logger = require('../util/Logger').getLogger(__filename);
 
 module.exports = {
   init: () => {
     if (config_common.is_testing) {
+      logger.info('on development environment');
+      logger.info('loading additional dotenv stuffs');
       require('dotenv').config();
+    }
+    else {
+      logger.info('on production environment');
+      logger.info('using AWS environment variables');
     }
   },
   config: config_common.is_testing ? require('./config_dev.json') : require('./config_prod.json'),
