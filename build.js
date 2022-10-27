643d3ea50253451d5ec7b6767e5c225029c5f758
Runas8128 <runas8128@naver.com>
2022-10-28 07:34:22 +0900

    fix some issues

--- build.js
+++ build.js
@@ -1,35 +1,32 @@
 const fs = require('fs');
 const ZIP = require('jszip');
 const path = require('path');
-const zip = new ZIP();
 
 const ignore_folder = ['.git', '.vscode', 'node_modules'];
-const ignore_file = ['.env', '.eslintrc', '.gitignore', 'README.md', 'test.js', 'build.js', 'Ringon.zip'];
+const ignore_file = ['.eslintrc', '.gitignore', 'README.md', 'test.js', 'build.js', 'Ringon.zip'];
 
 /**
- * @param {string} root
+ * @param {ZIP} zip
+ * @param {string} path_
  */
-function add_zip(root) {
-  console.log(`zipping ${root}`);
-  const dir_list = fs.readdirSync(root, { withFileTypes: true });
+function add_zip(zip, path_) {
+  console.log(`zipping ${path_}`);
+  const dir_list = fs.readdirSync(path_, { withFileTypes: true });
 
   dir_list
     .filter(dir => dir.isFile() && !ignore_file.includes(dir.name))
     .forEach(dir => {
-      const file = path.join(root, dir.name);
-      zip.file(file, fs.readFileSync(file));
+      const file = path.join(path_, dir.name);
+      zip.file(dir.name, fs.readFileSync(file));
     });
 
   dir_list
     .filter(dir => dir.isDirectory() && !ignore_folder.includes(dir.name))
-    .forEach(dir => add_zip(path.join(root, dir.name)));
+    .forEach(dir => add_zip(zip.folder(dir.name), path.join(path_, dir.name)));
 
   return zip;
 }
 
-add_zip('.')
-  .generateNodeStream({ type: 'nodebuffer', streamFiles: true })
-  .pipe(fs.createWriteStream('Ringon.zip'))
-  .on('finish', () => {
-    console.log('Done!');
-  });
+add_zip(new ZIP(), '.')
+  .generateAsync({ type: 'nodebuffer', compression: 'DEFLATE' })
+  .then((content) => { fs.writeFileSync('Ringon.zip', content); });
