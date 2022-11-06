ab87ca2152d8869fbb4e92d0955f77b3f1c0b0f3
Runas8128 <runas8128@naver.com>
2022-11-06 21:05:58 +0900

    remove database info from index page

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,5 @@
 const { RequestHandler } = require('express');
 
-const { config_common } = require('../../config');
-const manager = require('../../database');
 const logger = require('../../util/Logger').getLogger(__filename);
 
 module.exports = {
@@ -11,35 +9,6 @@ module.exports = {
   /** @type {RequestHandler} */
   get: (req, resp) => {
     logger.info('GET /');
-    resp.render('index', {
-      sync_data: Object.keys(manager.last_sync)
-        .map(db_name => ({
-          name: db_name,
-          last_sync: parse_timestamp(manager.last_sync[db_name]),
-          state: get_state(db_name),
-        })),
-    });
+    resp.render('index');
   },
 };
-
-function get_state(db_name) {
-  if (manager.last_sync[db_name] == 0) {
-    return 'not synced';
-  }
-
-  if (Date.now() - manager.last_sync > config_common.databases[db_name] * 3600000) {
-    return 'outdated';
-  }
-
-  return 'OK';
-}
-
-/**
- * @param {number} timestamp
- * @param {string} _default
- */
-function parse_timestamp(timestamp, _default) {
-  if (timestamp == 0) return _default;
-  const target_date = new Date(timestamp + 9 * 60 * 60 * 1000);
-  return target_date.toISOString().replace('T', ' ').slice(0, -5);
-}
