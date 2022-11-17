0f37283b8279713196b1cc48c9b5f8328f14577d
Runas8128 <runas8128@naver.com>
2022-11-17 13:38:50 +0900

    remove unused functions and require

--- util/index.js
+++ util/index.js
@@ -1,10 +1,6 @@
 const { RequestTimeoutError, APIResponseError, APIErrorCode } = require('@notionhq/client');
 const { ChatInputCommandInteraction, MessagePayload, InteractionReplyOptions } = require('discord.js');
 
-const isRetryable = error =>
-  (error instanceof RequestTimeoutError) ||
-  (error instanceof APIResponseError && error.code == APIErrorCode.RateLimited);
-
 /**
  * @param {ChatInputCommandInteraction} interaction
  * @param {string|MessagePayload|InteractionReplyOptions} msg
@@ -12,44 +8,6 @@ const isRetryable = error =>
 const reply = (interaction, msg) =>
   interaction[interaction.replied || interaction.deferred ? 'editReply' : 'reply'](msg);
 
-/**
- * @param {ChatInputCommandInteraction} interaction
- * @param {() => Promise<void>} callback
- * @param {Number} try_count
- * @returns {Promise<boolean>}
- */
-async function catch_timeout(interaction, callback, try_count) {
-  if (try_count > 5) {
-    reply(interaction, {
-      content: '재시도 횟수가 5회를 초과했습니다. 요청을 취소합니다.',
-      ephemeral: true,
-    });
-    return false;
-  }
-  try {
-    try_count = (try_count ?? 0) + 1;
-    await callback();
-    return true;
-  }
-  catch (err) {
-    if (isRetryable(err)) {
-      reply(interaction, {
-        content: `요청에 실패했습니다. 재시도중... (${try_count} / 5)`,
-        ephemeral: true,
-      });
-
-      await timer(100);
-      return catch_timeout(interaction, callback, try_count);
-    }
-    else {
-      throw err;
-    }
-  }
-}
-
 const timer = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-module.exports = {
-  catch_timeout: catch_timeout,
-  timer: timer,
-};
+module.exports = { timer, reply };
