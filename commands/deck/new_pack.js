3b6bd623db88dc713c67dc9316ca53da680b1f4d
Runas8128 <runas8128@naver.com>
2023-06-22 07:38:03 +0900

    Merge commit 'a8f48d09abe7118ec0f8728103754838113233f4'

--- commands/deck/new_pack.js
+++ commands/deck/new_pack.js
@@ -1,8 +1,11 @@
-const { SlashCommandBuilder, ChatInputCommandInteraction } = require('discord.js');
+const { SlashCommandBuilder, ChatInputCommandInteraction, ButtonStyle } = require('discord.js');
 const { ActionRowBuilder, ButtonBuilder, EmbedBuilder, ComponentType } = require('discord.js');
 
 const { decklist } = require('../../database');
 const { reply } = require('../../util');
+const Logger = require('../../util/Logger');
+
+const logger = Logger.getLogger(__filename);
 
 module.exports = {
   perm: 'admin',
@@ -15,30 +18,21 @@ module.exports = {
    * @param {ChatInputCommandInteraction} interaction
    */
   async execute(interaction) {
-    const timestamp = Date.now();
-
     await reply(interaction, {
       embeds: [new EmbedBuilder()
         .setTitle('⚠️ 해당 명령어 사용시, 현재 등록된 덱리가 모두 삭제됩니다.')
         .setDescription('사용하시려면 `확인`을 입력해주세요! 1분 후 자동으로 취소됩니다.'),
       ],
-      components: [new ActionRowBuilder()
-        .addComponents(
-          new ButtonBuilder()
-            .setCustomId(`PackChecker_${timestamp}`)
-            .setLabel('확인'),
-        ),
-      ],
     });
 
     try {
-      const checker = await interaction.channel.awaitMessageComponent({
-        componentType: ComponentType.Button,
+      await interaction.channel.awaitMessages({
         time: 60 * 1000,
-        filter: (button) => button.customId == `PackChecker_${timestamp}`,
+        filter: msg => msg.author.id === interaction.user.id && msg.content === '확인',
       });
 
-      await checker.deferUpdate();
+      interaction.followUp('승인되었습니다! 덱리 초기화 및 팩이름 변경을 실행합니다...');
+
       decklist.update_pack(
         interaction.options.getString('이름'),
         interaction.guild,
